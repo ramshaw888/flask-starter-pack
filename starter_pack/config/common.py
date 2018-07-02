f78b2b221897201de68a87218b72eb96f9a6b4c5
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2018-07-02 14:07:52 +1000

    Add DEBUG, TESTING config values in their respective environment config modules

--- starter_pack/config/common.py
+++ starter_pack/config/common.py
@@ -1,2 +1,4 @@
 class Config(object):
     ENVIRONMENT = None
+    DEBUG = False
+    TESTING = False
--- starter_pack/config/dev.py
+++ starter_pack/config/dev.py
@@ -3,3 +3,5 @@ from starter_pack.config.common import Config as CommonConfig
 
 class Config(CommonConfig):
     ENVIRONMENT = 'dev'
+    DEBUG = True
+    TESTING = True
