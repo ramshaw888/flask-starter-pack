2b9884e2ede7cea4c2f32b94507ca7a2e5af0255
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2018-09-27 16:51:14 +1000

    Add ./run.sh pip command to build Pipfile.lock back into the repo

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,8 @@
 FROM python:3.7.0
-RUN pip install --user pipenv
-WORKDIR ~
-ADD Pipfile.lock .
-ADD Pipfile .
-ENV PATH="/root/.local/bin:${PATH}"
+RUN pip install pipenv
+WORKDIR /root
+ADD Pipfile* /root/
 RUN pipenv install --three
 ADD run.py .
 ADD starter_pack starter_pack
 CMD pipenv run python run.py
-
-#ADD starter_pack /starter_pack
--- run.sh
+++ run.sh
@@ -3,12 +3,13 @@
 function default {
   docker build --tag starter-pack .
   docker network create starter-pack-local
-  docker run -it --network ramshaw_network --publish 5100:5100 starter-pack
+  docker run -it --network ramshaw_network --publish 5100:5100 starter-pack bash
 }
 
 case $1 in
   "pip")
-    echo "todo"
+    docker build --tag starter-pack .
+    docker run -it starter-pack cat Pipfile.lock > Pipfile.lock
     ;;
   *)
     default
