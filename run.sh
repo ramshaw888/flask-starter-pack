23417104bc21ab9106a3945a33c7f8281233f4bc
Aaron Ramshaw <aaron.ramshaw@domain.com.au>
2019-03-27 10:43:35 +1100

    Split out management dockerfile

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #/bin/bash
 
 APP_NAME=${APP_NAME:="starter_pack"}
+MANAGEMENT_APP_NAME="$APP_NAME"_management
 
 docker ps -a -q --filter name=${APP_NAME} | xargs -I {} sh -c 'docker stop {}; docker rm -f {}'
 
 function build {
   docker build --tag ${APP_NAME} .
+}
+
+function build_management {
+  docker build -f Dockerfile.management --tag ${MANAGEMENT_APP_NAME} .
 }
 
 function default {
@@ -17,13 +22,13 @@ function default {
 }
 
 function lint {
-  build
-  docker run -v $(pwd):/root -it ${APP_NAME} bash -c 'pip install flake8 && flake8'
+  build_management
+  docker run -v $(pwd):/root -it ${MANAGEMENT_APP_NAME} flake8
 }
 
 function pip_compile {
-  build
-  docker run -v $(pwd):/root -it ${APP_NAME} bash -c 'pip install pip-tools && pip-compile'
+  build_management
+  docker run -v $(pwd):/root -it ${MANAGEMENT_APP_NAME} pip-compile
 }
 
 case $1 in
